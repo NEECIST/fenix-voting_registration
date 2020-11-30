c4f42808110a82671afe2fa5cf03fcac754c7d51
Carlos-Marques <carlosmarques.personal@gmail.com>
2020-11-30 21:57:56 +0000

    rough initial version

---
+++ app/models.py
@@ -0,0 +1,92 @@
+from app import db
+from sqlalchemy.orm import relationship
+
+class AcademicTerm(db.Model):
+    __tablename__ = "academicTerms"
+    id = db.Column(db.Integer, primary_key=True)
+    
+    registration_id = db.Column(db.Integer, db.ForeignKey('registrations.id'))
+
+    term = db.Column(db.String)
+
+class Registration(db.Model):
+    __tablename__ = "registrations"
+    id = db.Column(db.Integer, primary_key=True)
+
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    
+    registration_id = db.Column(db.String)
+    name = db.Column(db.String)
+    acronym = db.Column(db.String)
+    academicTerms = relationship("AcademicTerm") 
+
+class Role(db.Model):
+    __tablename__ = "roles"
+    id = db.Column(db.Integer, primary_key=True)
+
+    person_id = db.Column(db.Integer, db.ForeignKey('persons.id'))
+
+    type = db.Column(db.String)
+    registrations = relationship("Registration")
+
+class Photo(db.Model):
+    __tablename__ = "photos"
+    id = db.Column(db.Integer, primary_key=True)
+
+    person_id = db.Column(db.Integer, db.ForeignKey('persons.id'))
+    person = relationship("Person", back_populates="photo")
+
+    type = db.Column(db.String)
+    data = db.Column(db.Text)
+
+class PersonalEmail(db.Model):
+    __tablename__ = "personalEmails"
+    id = db.Column(db.Integer, primary_key=True)
+
+    person_id = db.Column(db.Integer, db.ForeignKey('persons.id'))
+
+    email = db.Column(db.String)
+
+class WorkEmail(db.Model):
+    __tablename__ = "workEmails"
+    id = db.Column(db.Integer, primary_key=True)
+
+    person_id = db.Column(db.Integer, db.ForeignKey('persons.id'))
+
+    email = db.Column(db.String)
+
+class WebAddress(db.Model):
+    __tablename__ = "webAddresses"
+    id = db.Column(db.Integer, primary_key=True)
+
+    person_id = db.Column(db.Integer, db.ForeignKey('persons.id'))
+
+    url = db.Column(db.String)
+
+class WorkWebAddress(db.Model):
+    __tablename__ = "workWebAddresses"
+    id = db.Column(db.Integer, primary_key=True)
+    
+    person_id = db.Column(db.Integer, db.ForeignKey('persons.id'))
+
+    url = db.Column(db.String)
+
+class Person(db.Model):
+    __tablename__ = "persons"
+    id = db.Column(db.Integer, primary_key=True)
+
+    campus = db.Column(db.String)
+    name = db.Column(db.String)
+    gender = db.Column(db.String)
+    birthday = db.Column(db.Date)
+    username = db.Column(db.String)
+    email = db.Column(db.String)
+    displayName = db.Column(db.String)
+    institutionalEmail = db.Column(db.String)
+
+    roles = relationship("Role")
+    photo = relationship("Photo", uselist=False, back_populates="person")
+    personalEmails = relationship("PersonalEmail")
+    workEmails = relationship("WorkEmail")
+    webAddresses = relationship("WebAddress")
+    workWebAddresses = relationship("WorkWebAddress")
---
+++ app/routes.py
@@ -0,0 +1,72 @@
+from flask import request
+from datetime import datetime
+import fenixedu
+
+from app import app, db
+from app.models import *
+
+config = fenixedu.FenixEduConfiguration.fromConfigFile('fenixedu.ini')
+client = fenixedu.FenixEduClient(config)
+
+@app.route('/')
+def index():
+    CODE = request.args.get('code')
+    user = client.get_user_by_code(CODE)
+    person = client.get_person(user)
+    
+    birthday = datetime.strptime(person["birthday"], "%d/%m/%Y")
+    
+    p = Person(campus=person["campus"], name=person["name"], gender=person["gender"], username=person["username"], email=person["email"], displayName=person["displayName"], institutionalEmail=person["institutionalEmail"], birthday=birthday)
+
+    photo = Photo(type=person["photo"]["type"], data=person["photo"]["data"])
+    p.photo = photo
+
+    personalEmails = []
+    for email in person["personalEmails"]:
+        personalEmails.append(email)
+
+    p.personalEmails = personalEmails
+
+    workEmails = []
+    for email in person["workEmails"]:
+        workEmails.append(email)
+
+    p.workEmails = workEmails
+
+    webAddresses = []
+    for url in person["webAddresses"]:
+        webAddresses.append(url)
+
+    p.webAddresses = webAddresses
+
+    workWebAddresses = []
+    for url in person["workWebAddresses"]:
+        workWebAddresses.append(url)
+
+    p.workWebAddresses = workWebAddresses
+
+    roles = []
+    for role in person["roles"]:
+        r = Role(type=role["type"])
+        
+        registrations = []
+        for registration in role["registrations"]:
+            reg = Registration(registration_id=registration["id"], name=registration["name"], acronym=registration["acronym"])
+
+            terms = []
+            for term in registration["academicTerms"]:
+                t = AcademicTerm(term=term)
+                terms.append(t)
+
+            reg.academicTerms = terms
+            registrations.append(reg)
+        
+        r.registrations = registrations
+        roles.append(r)
+
+    p.roles = roles
+
+    db.session.add(p)
+    db.session.commit()
+
+    return "Obrigado pelo registo!"
